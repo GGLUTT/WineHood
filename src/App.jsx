5320feac47dd3ca4f9a0974a0173a0777421a7bc
luter <newgglutt@gmail.com>
2025-05-06 18:59:03 +0300

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import { CartProvider } from './components/Context/CartContext.jsx';
 import ForgotPasswordPage from './components/Header/ForgotPassword/ForgotPasswordPage';
@@ -50,7 +50,7 @@ function AppContent() {
 function App() {
   return (
     <CartProvider>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <AppContent />
       </Router>
     </CartProvider>
